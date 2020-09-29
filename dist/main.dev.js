515adcc116ec5ffebdc8301d4b797c0c56a0f120
Sophie Roe <sophie.roe@Nology.local>
2020-09-29 18:25:08 +0100

    draft 29.09

---
+++ dist/main.dev.js
@@ -0,0 +1,7 @@
+//insert 10 times bomb random function
+//insert numbers - 3 is 3 adjaccent, 2 if 2 adjacent, 1 if 1, grey if 0
+// if click on bomb, reveal all bombs, end game - you lost pop up, play again option
+// if 3,2,1 or 0, reveal square
+//game ends when all grey space has been identified or all flags in correct position - you win popup
+////to ask sam how to do function so they are relative to each other?
+"use strict";
